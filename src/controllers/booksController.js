86602571f86bbeea06cb67d4ceab4d2b93252693
Mateo Arismendy <arismendyuribemateo@gmail.com>
2025-04-16 15:14:35 -0400

    hanlde errors

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -2,65 +2,80 @@ import pool from "../db.js";
 
 //* GET FUNCTIONS ----------------------------------------------------------
 const getAllBooks = async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM books");
-  res.status(200).json({
-    message: "Books fetched successfully",
-    books: rows,
-  });
+  try {
+    const [rows] = await pool.query("SELECT * FROM books");
+    res.status(200).json({
+      message: "Books fetched successfully",
+      books: rows,
+    });
+  } catch (error) {
+    console.error("Error while fetching books:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 const getBookById = async (req, res) => {
-  const id = req.params.id;
-  const [rows] = await pool.query("SELECT * FROM books WHERE id = ?", [id]);
+  try {
+    const id = req.params.id;
+    const [rows] = await pool.query("SELECT * FROM books WHERE id = ?", [id]);
 
-  if (rows.length === 0) {
-    return res
-      .status(404)
-      .json({ message: `Book with id ${id} has not been found.` });
+    if (rows.length === 0) {
+      return res
+        .status(404)
+        .json({ message: `Book with id ${id} has not been found.` });
+    }
+
+    res.status(200).json({
+      message: "Books fetched successfully",
+      book: rows[0],
+    });
+  } catch (error) {
+    console.error(`Error while fetching book with id ${id}:`, error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-
-  res.status(200).json({
-    message: "Books fetched successfully",
-    book: rows[0],
-  });
 };
 
 const getGroupedBooks = async (req, res) => {
-  //Ref to get decade -- https://stackoverflow.com/questions/65915114/having-issues-summarizing-years-as-decades-in-mysql
-  const [rows] = await pool.query(
-    `SELECT 10*FLOOR(published_year/10) AS decade, 
+  try {
+    //Ref to get decade -- https://stackoverflow.com/questions/65915114/having-issues-summarizing-years-as-decades-in-mysql
+    const [rows] = await pool.query(
+      `SELECT 10*FLOOR(published_year/10) AS decade, 
       id, title, author_id, published_year
       FROM books 
       ORDER BY title ASC`
-  );
+    );
 
-  if (rows.length === 0) {
-    return res.status(404).json({ message: `Books have not been found.` });
-  }
-
-  let groupedBooks = {};
-
-  for (const book of rows) {
-    const decade = `${book.decade}s`;
-
-    //If decate does not exist yet, then create it
-    if (!groupedBooks[decade]) {
-      groupedBooks[decade] = [];
+    if (rows.length === 0) {
+      return res.status(404).json({ message: `Books have not been found.` });
     }
 
-    //Else push the book to the decade
-    groupedBooks[decade].push({
-      id: book.id,
-      title: book.title,
-      author_id: book.author_id,
-      published_year: book.published_year,
-    });
-  }
+    let groupedBooks = {};
 
-  res.status(200).json({
-    message: "Books fetched successfully",
-    data: groupedBooks,
-  });
+    for (const book of rows) {
+      const decade = `${book.decade}s`;
+
+      //If decate does not exist yet, then create it
+      if (!groupedBooks[decade]) {
+        groupedBooks[decade] = [];
+      }
+
+      //Else push the book to the decade
+      groupedBooks[decade].push({
+        id: book.id,
+        title: book.title,
+        author_id: book.author_id,
+        published_year: book.published_year,
+      });
+    }
+
+    res.status(200).json({
+      message: "Books fetched successfully",
+      data: groupedBooks,
+    });
+  } catch (error) {
+    console.error("Error while fetching grouped books:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 const getBookWithAuthorData = async (req, res) => {
@@ -80,7 +95,7 @@ const getBookWithAuthorData = async (req, res) => {
         const authorData = await response.json();
 
         return {
-          book_id: book.id,
+          id: book.id,
           title: book.title,
           author_id: book.author_id,
           published_year: book.published_year,
@@ -90,7 +105,7 @@ const getBookWithAuthorData = async (req, res) => {
         //If there is an error fetching the external API then return book with null author_data
         console.error(`Error fetching author for book ${book.id}:`, error);
         return {
-          book_id: book.id,
+          id: book.id,
           title: book.title,
           author_id: book.author_id,
           published_year: book.published_year,
@@ -106,71 +121,86 @@ const getBookWithAuthorData = async (req, res) => {
       data: booksWithAuthorData,
     });
   } catch (error) {
-    console.error("Error in getBookWithAuthorData:", error);
+    console.error("Error while getting all books:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 //* POST FUNCTIONS ----------------------------------------------------------
 const createBook = async (req, res) => {
-  const { title, author_id, published_year } = req.body;
-  const [result] = await pool.query(
-    "INSERT INTO books (title, author_id, published_year) VALUES (?, ?, ?)",
-    [title, author_id, published_year]
-  );
+  try {
+    const { title, author_id, published_year } = req.body;
+    const [result] = await pool.query(
+      "INSERT INTO books (title, author_id, published_year) VALUES (?, ?, ?)",
+      [title, author_id, published_year]
+    );
 
-  res.status(201).json({
-    message: "Book created successfully",
-    book: {
-      id: result.insertId,
-      title,
-      author_id,
-      published_year,
-    },
-  });
+    res.status(201).json({
+      message: "Book created successfully",
+      data: {
+        id: result.insertId,
+        title,
+        author_id,
+        published_year,
+      },
+    });
+  } catch (error) {
+    console.error("Error while creating book:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 //* PUT FUNCTIONS ----------------------------------------------------------
 const updateBook = async (req, res) => {
-  const id = req.params.id;
-  const { title, author_id, published_year } = req.body;
+  try {
+    const id = req.params.id;
+    const { title, author_id, published_year } = req.body;
 
-  const [result] = await pool.query(
-    "UPDATE books SET title = ?, author_id = ?, published_year = ? WHERE id = ?",
-    [title, author_id, published_year, id]
-  );
+    const [result] = await pool.query(
+      "UPDATE books SET title = ?, author_id = ?, published_year = ? WHERE id = ?",
+      [title, author_id, published_year, id]
+    );
 
-  if (result.affectedRows === 0) {
-    return res
-      .status(404)
-      .json({ message: `Book with id ${id} has not been found.` });
+    if (result.affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ message: `Book with id ${id} has not been found.` });
+    }
+
+    res.status(200).json({
+      message: `Book with id ${id} has been updated successfully`,
+      data: {
+        id,
+        title,
+        author_id,
+        published_year,
+      },
+    });
+  } catch (error) {
+    console.error("Error while updating book:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-
-  res.status(200).json({
-    message: `Book with id ${id} has been updated successfully`,
-    book: {
-      id,
-      title,
-      author_id,
-      published_year,
-    },
-  });
 };
 
 //* DELETE FUNCTIONS ----------------------------------------------------------
 const deleteBook = async (req, res) => {
-  const id = req.params.id;
-  const [result] = await pool.query("DELETE FROM books WHERE id = ?", [id]);
+  try {
+    const id = req.params.id;
+    const [result] = await pool.query("DELETE FROM books WHERE id = ?", [id]);
 
-  if (result.affectedRows === 0) {
-    return res
-      .status(404)
-      .json({ message: `Book with id ${id} has not been found.` });
+    if (result.affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ message: `Book with id ${id} has not been found.` });
+    }
+
+    res.status(200).json({
+      message: `Book with id ${id} has been deleted successfully`,
+    });
+  } catch (error) {
+    console.error("Error while deleting book:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-
-  res.status(200).json({
-    message: `Book with id ${id} has been deleted successfully`,
-  });
 };
 
 export {
